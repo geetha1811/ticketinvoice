7ccd980197a3da56d931d812b78c3759d3885b8d
geetha1811 <142973155+geetha1811@users.noreply.github.com>
2024-09-25 22:19:49 +0530

    Create sidebar-page2.component.ts

---
+++ sidebar-page2.component.ts
@@ -0,0 +1,11 @@
+// src/app/sidebar-page2/sidebar-page2.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-page2',
+  templateUrl: './sidebar-page2.component.html',
+  styleUrls: ['./sidebar-page2.component.css']
+})
+export class SidebarPage2Component {
+  constructor() {}
+}
